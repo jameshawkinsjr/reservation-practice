5721151b3d63136dc2e28fcb0836a7ebb29472e3
James <james@jameshawkins.com>
2019-06-19 14:14:04 -0700

    Add dropdown javascript

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ Date.prototype.toDateInputValue = (function() {
     return local.toJSON().slice(0,10);
 });
 
-let getRoundedTimeString = () => {
-    let milliseconds = 1000 * 60 * 30; // Nearest 30 minutes
+let getRoundedTimeString = (minutes) => {
+    let milliseconds = 1000 * 60 * minutes; // Nearest 30 minutes
     return new Date(Math.round(new Date().getTime() / milliseconds) * milliseconds)
         .toLocaleTimeString('it-IT', {hour: '2-digit', minute:'2-digit'});
   };
-
 // Set default values for inputs
 document.getElementById('dateInput').value = new Date().toDateInputValue();
-document.getElementById("timeInput").defaultValue = getRoundedTimeString();
+document.getElementById("timeInput").defaultValue = getRoundedTimeString(30);
+
+let addDropdown = () => {
+    let dropdown = document.getElementById("dropdown");
+    dropdown.classList.remove("hidden");
+}
+
+let signupForm = () => {
+    let dropdown = document.getElementById("signup-form");
+    dropdown.classList.remove("hidden");
+}
+
